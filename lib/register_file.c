00c621438a7a3377483ba4b1faa3cc72ab1ca038
leukelele <70491331+leukelele@users.noreply.github.com>
2025-08-14 13:51:11 -0500

    uppercase hexadecimals

--- lib/register_file.c
+++ lib/register_file.c
@@ -2,10 +2,10 @@
 
 void reg_init(reg_file *reg_file) {
     // init registers according to the original bootstrap rom
-    reg_file->af.reg = 0x01b0;
+    reg_file->af.reg = 0x01B0;
     reg_file->bc.reg = 0x0013;
-    reg_file->de.reg = 0x00d8;
-    reg_file->hl.reg = 0x014d;
-    reg_file->sp     = 0xfffe;
+    reg_file->de.reg = 0x00D8;
+    reg_file->hl.reg = 0x014D;
+    reg_file->sp     = 0xFFFE;
     reg_file->pc     = 0x0100;
 }
