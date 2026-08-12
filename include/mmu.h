83e755117de333f414c9a13c14b809d2cf12990e
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-12 16:40:31 -0500

    init new mmu

--- include/mmu.h
+++ include/mmu.h
@@ -13,8 +13,8 @@
  * This abstraction ensures that the CPU implementation remains independent
  * of the underlying memory layout and hardware mappings.
  */
-#ifndef BUS_H
-#define BUS_H
+#ifndef MMU_H
+#define MMU_H
 
 #include <stdint.h>
 
--- lib/mmu.c
+++ lib/mmu.c
@@ -4,5 +4,7 @@
 uint8_t bus_read8(bus_t *bus, uint16_t addr) {
     if (addr <= 0x7FFF) return 0x00; // cart ROM, though stubbed due to no
                                      // cartridge loader yet
+    else if (addr <= 0x9FFF) return bus->vram[addr - 0x8000];
+
     return 0x00;
 }
