f4c7cd2c69bdc24a47c10fc9faa77bac7468e438
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-13 15:07:33 -0500

    creating a struct allows for main to hold onto CPU's necessities until end of program

--- include/cpu.h
+++ include/cpu.h
@@ -2,6 +2,13 @@
 #define CPU_H
 
 #include <stdint.h>
+#include "register_file.h"
+#include "mmu.h"
+
+struct cpu {
+    struct reg_file regs;
+    struct bus *bus;
+};
 
 void    cpu_init(void);
 uint8_t fetch(void);
