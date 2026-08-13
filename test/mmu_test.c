4cfa8683a903f79ef0cba5133e722199db64fb19
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-13 13:52:44 -0500

    test for mmu

---
+++ test/mmu_test.c
@@ -0,0 +1,38 @@
+#include "unity.h"
+#include "mmu.h"
+
+// setUp and tearDown are required, even if empty
+void setUp(void) {}
+void tearDown(void) {}
+
+void test_vram_round_trip(void) {
+    bus_t bus = {0};
+    bus_write8(&bus, 0x8000, 0x42);     // first byte of VRAM
+    TEST_ASSERT_EQUAL_UINT8(0x42, bus_read8(&bus, 0x8000));
+
+    bus_write8(&bus, 0x9FFF, 0x99);     // last byte of VRAM
+    TEST_ASSERT_EQUAL_UINT8(0x99, bus_read8(&bus, 0x9FFF));
+}
+
+void test_echo_ram_mirrors_wram(void) {
+    bus_t bus = {0};
+    bus_write8(&bus, 0xC005, 0xAB);     // write through WRAM
+    TEST_ASSERT_EQUAL_UINT8(0xAB, bus_read8(&bus, 0xE005)); // read echo
+
+    bus_write8(&bus, 0xE010, 0xCD);
+    TEST_ASSERT_EQUAL_UINT8(0xCD, bus_read8(&bus, 0xC010));
+}
+
+void test_rom_stub_reads_zero(void) {
+    bus_t bus = {0};
+    TEST_ASSERT_EQUAL_UINT8(0x00, bus_read8(&bus, 0x0000));
+    TEST_ASSERT_EQUAL_UINT8(0x00, bus_read8(&bus, 0x7FFF));
+}
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_vram_round_trip);
+    RUN_TEST(test_echo_ram_mirrors_wram);
+    RUN_TEST(test_rom_stub_reads_zero);
+    return UNITY_END();
+}
