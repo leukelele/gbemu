96f9928da503fed6af08583cb9df796f45365c5f
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-18 13:52:18 -0500

    i change my mind, it may be useful to have these functions accessible cross file. I also think the CPU should provide means for retrieving and updating register values

--- include/cpu.h
+++ include/cpu.h
@@ -40,6 +40,38 @@ struct cpu {
 void cpu_init(struct cpu *cpu, struct bus *bus);
 
 /**
+ * The function is basically a wrapper for the CPU struct and its bus 
+ * connection. This progresses the program counter (PC)
+ *
+ * @param[in] cpu It is the CPU's bus which is to be read along with the
+ *                current register pointer.
+ * @return The address found at the program counter.
+ */
+uint8_t fetch(struct cpu *cpu);
+
+/**
+ * Essentially an instruction table lookup wrapper. It's more for 
+ * conceptualization.
+ *
+ * @param[in] opcode The opcode to be translated.
+ * @return The particular instruction struct that the opcode translates to.
+ */
+const struct instruction *decode(uint8_t opcode);
+
+/**
+ * Checking that the instruction routine exists, executes the instruction
+ * routine.
+ *
+ * @param[in] cpu This is for the CPU's registers
+ * @param[in] inst The instruction struct itself contains the means for 
+ *                 executing that particular instruction.
+ * @return The machine cycles as this is the only way to know how long the
+ *         instruction will execute.
+ */
+uint8_t execute(struct cpu *cpu, const struct instruction *inst,
+                        uint8_t opcode);
+
+/**
  * Bundles the complete CPU loop: fetch -> decode -> execute.
  *
  * @param[in] cpu This is for the CPU's registers
@@ -48,5 +80,9 @@ void cpu_init(struct cpu *cpu, struct bus *bus);
  *         instruction will execute.
  */
 uint8_t cpu_step(struct cpu *cpu);
+
+
+uint8_t reg8_get(struct cpu *cpu, uint8_t index);
+void reg8_set(struct cpu *cpu, uint8_t index, uint8_t value);
 
 #endif
