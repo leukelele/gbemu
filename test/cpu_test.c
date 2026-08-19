fc1863d0a341f4f671f5e2d23b509b451dbbb979
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-19 13:43:53 -0500

    update old tests and added new test to check ld rr nn and ld r n

--- test/cpu_test.c
+++ test/cpu_test.c
@@ -37,7 +37,7 @@ void test_decode_returns_correct_metadata_for_ld_bc_indirect_a(void) {
 
 void test_decode_returns_null_execute_for_unimplemented_opcode(void) {
     instruction_set_init();
-    const struct instruction *inst = decode(0x01); // never populated
+    const struct instruction *inst = decode(0x03); // not yet populated
 
     TEST_ASSERT_NULL(inst->execute);
 }
@@ -48,8 +48,8 @@ void test_execute_nop_returns_mach_cycles(void) {
     struct cpu cpu;
     cpu_init(&cpu, &bus);
 
-//    uint8_t cycles = execute(&cpu, decode(0x00));
-//    TEST_ASSERT_EQUAL_UINT8(1, cycles);
+    uint8_t cycles = execute(&cpu, decode(0x00), 0x00);
+    TEST_ASSERT_EQUAL_UINT8(1, cycles);
 }
 
 void test_execute_ld_bc_indirect_a_writes_a_to_bc_address(void) {
@@ -59,12 +59,12 @@ void test_execute_ld_bc_indirect_a_writes_a_to_bc_address(void) {
     cpu_init(&cpu, &bus);
 
     cpu.regs.bc.pair = 0xC010;      // arbitrary WRAM address
-    cpu.regs.af.byte.hi = 0x99;   // register A
+    cpu.regs.af.byte.hi = 0x99;     // register A
 
-//    uint8_t cycles = execute(&cpu, decode(0x02));
-//
-//    TEST_ASSERT_EQUAL_UINT8(0x99, bus_read8(&bus, 0xC010));
-//    TEST_ASSERT_EQUAL_UINT8(2, cycles);
+    uint8_t cycles = execute(&cpu, decode(0x02), 0x02);
+
+    TEST_ASSERT_EQUAL_UINT8(0x99, bus_read8(&bus, 0xC010));
+    TEST_ASSERT_EQUAL_UINT8(2, cycles);
 }
 
 void test_execute_unimplemented_opcode_returns_zero(void) {
@@ -73,9 +73,9 @@ void test_execute_unimplemented_opcode_returns_zero(void) {
     struct cpu cpu;
     cpu_init(&cpu, &bus);
 
-//    uint8_t cycles = execute(&cpu, decode(0x01));
-//
-//    TEST_ASSERT_EQUAL_UINT8(0, cycles);
+    uint8_t cycles = execute(&cpu, decode(0xD3), 0xD3);
+
+    TEST_ASSERT_EQUAL_UINT8(0, cycles);
 }
 
 void test_cpu_step_nop_advances_pc_by_one(void) {
@@ -91,6 +91,40 @@ void test_cpu_step_nop_advances_pc_by_one(void) {
 
     TEST_ASSERT_EQUAL_UINT16(pc_before + 1, cpu.regs.pc);
     TEST_ASSERT_EQUAL_UINT8(1, cycles);
+}
+
+void test_cpu_step_ld_rr_nn_loads_hl(void) {
+    instruction_set_init();
+    struct bus bus = {0};
+    struct cpu cpu;
+    cpu_init(&cpu, &bus);
+    cpu.regs.pc = 0xC000;
+
+    bus_write8(&bus, 0xC000, 0x21);
+    bus_write8(&bus, 0xC001, 0x34);
+    bus_write8(&bus, 0xC002, 0x12);
+
+    uint8_t cycles = cpu_step(&cpu);
+
+    TEST_ASSERT_EQUAL_UINT16(0x1234, cpu.regs.hl.pair);
+    TEST_ASSERT_EQUAL_UINT16(0xC003, cpu.regs.pc);
+    TEST_ASSERT_EQUAL_UINT16(3, cycles);
+}
+
+void test_cpu_step_ld_r_n_loads_register(void) {
+    instruction_set_init();
+    struct bus bus = {0};
+    struct cpu cpu;
+    cpu_init(&cpu, &bus);
+    cpu.regs.pc = 0xC000;
+
+    bus_write8(&bus, 0xC000, 0x06);
+    bus_write8(&bus, 0xC001, 0x5A);
+
+    uint8_t cycles = cpu_step(&cpu);
+
+    TEST_ASSERT_EQUAL_UINT16(0x5A, cpu.regs.bc.byte.hi);
+    TEST_ASSERT_EQUAL_UINT16(2, cycles);
 }
 
 void test_cpu_step_ld_bc_indirect_a_writes_memory_and_advances_pc_by_one(void) {
