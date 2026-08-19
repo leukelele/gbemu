8412a0b17aa5caffc88cce626260b2e27b25615a
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-19 11:06:03 -0500

    init code for rest of the load instructions

--- lib/instruction_set.c
+++ lib/instruction_set.c
@@ -1,5 +1,6 @@
 #include "instruction_set.h"
 #include "cpu.h"
+#include "flags.h"
 
 static struct instruction inst_table[INSTRUCTION_TABLE_SIZE];
 
@@ -26,7 +27,6 @@ static void init_inst(uint8_t opcode, enum inst_fmt format,
  * load instructions       *
  ***************************/
 
-// LD r,r' block (0x40-0x7F, except 0x76=HALT)
 static bool exec_ld_r_r(struct cpu *cpu, uint8_t opcode) {
     uint8_t dst = (opcode >> 3) & 0x7;
     uint8_t src = opcode & 0x7;
@@ -57,6 +57,43 @@ static bool exec_ld_indirect_a(struct cpu *cpu, uint8_t opcode) {
     return false;
 }
 
+static bool exec_ld_nn_a(struct cpu *cpu, uint8_t opcode) {
+    bool load = (opcode >> 4) & 0x1;
+    uint8_t lo = fetch(cpu);
+    uint8_t hi = fetch(cpu);
+    uint16_t addr = (uint16_t)((hi << 8) | lo);
+
+    if (load) cpu->regs.af.byte.hi = bus_read8(cpu->bus, addr);
+    else      bus_write8(cpu->bus, addr, cpu->regs.af.byte.hi);
+    return false;
+}
+
+static bool exec_ld_sp_hl(struct cpu *cpu, uint8_t opcode) {
+    cpu->regs.sp = cpu->regs.hl.pair;
+    return false;
+}
+
+static bool exec_ld_hl_sp_e(struct cpu *cpu, uint8_t opcode) {
+    int8_t e = (int8_t)fetch(cpu);
+    uint16_t sp = cpu->regs.sp;
+
+    flag_set(cpu, FLAG_Z, false);
+    flag_set(cpu, FLAG_N, false);
+    flag_set(cpu, FLAG_H, ((sp & 0xF) + ((uint8_t)e & 0xF)) > 0xF);
+    flag_set(cpu, FLAG_C, ((sp & 0xFF) + (uint8_t)e) > 0xFF);
+
+    cpu->regs.hl.pair = (uint16_t)(sp + e);
+    return false;
+}
+
+static bool exec_ld_nn_sp(struct cpu *cpu, uint8_t opcode) {
+    uint8_t lo = fetch(cpu);
+    uint8_t hi = fetch(cpu);
+    uint16_t addr = (uint16_t)((hi << 8) | lo);
+    bus_write16(cpu->bus, addr, cpu->regs.sp);
+    return false;
+}
+
 static bool exec_ld_rr_nn(struct cpu *cpu, uint8_t opcode) {
     uint8_t lo = fetch(cpu);
     uint8_t hi = fetch(cpu);
@@ -82,15 +119,27 @@ static bool exec_ldh(struct cpu *cpu, uint8_t opcode) {
     return false;
 }
 
-static bool exec_pop_from_stack16(struct cpu *cpu, uint8_t opcode){
-    cpu->regs.bc.byte.lo = bus_read8(cpu->bus, cpu->regs.sp++);
-    cpu->regs.bc.byte.hi = bus_read8(cpu->bus, cpu->regs.sp++);
+static bool exec_push_rr(struct cpu *cpu, uint8_t opcode) {
+    uint16_t value;
+    switch ((opcode >> 4) & 0x3) {
+        case 0: value = cpu->regs.bc.pair; break;
+        case 1: value = cpu->regs.de.pair; break;
+        case 2: value = cpu->regs.hl.pair; break;
+        default: value = cpu->regs.af.pair; break;
+    }
+    stack_push16(cpu, value);
     return false;
 }
 
-static bool exec_push_to_stack16(struct cpu *cpu, uint8_t opcode) {
-    bus_write8(cpu->bus, --cpu->regs.sp, cpu->regs.bc.byte.hi);
-    bus_write8(cpu->bus, --cpu->regs.sp, cpu->regs.bc.byte.lo);
+static bool exec_pop_rr(struct cpu *cpu, uint8_t opcode) {
+    uint16_t value = stack_pop16(cpu);
+    switch ((opcode >> 4) & 0x3) {
+        case 0: cpu->regs.bc.pair = value; break;
+        case 1: cpu->regs.de.pair = value; break;
+        case 2: cpu->regs.hl.pair = value; break;
+        default: cpu->regs.af.pair = value & 0xFFF0; break; 
+                 // F's low nibble unwired
+    }
     return false;
 }
 
@@ -133,14 +182,22 @@ static void instruction_ld(void) {
         init_inst((uint8_t)opcode, LD, cycles, 0, exec_ld_r_r);
     }
 
+    init_inst(0xEA, LD, 4, 0, exec_ld_nn_a);
+    init_inst(0xFA, LD, 4, 0, exec_ld_nn_a);
+
+    init_inst(0xF9, LD, 2, 0, exec_ld_sp_hl);
+    init_inst(0xF8, LD, 3, 0, exec_ld_hl_sp_e);
+    init_inst(0x08, LD, 5, 0, exec_ld_nn_sp);
+
     init_inst(0xF0, LDH, 3, 0, exec_ldh);
     init_inst(0xE0, LDH, 3, 0, exec_ldh);
     init_inst(0xF2, LDH, 2, 0, exec_ldh);
     init_inst(0xE2, LDH, 2, 0, exec_ldh);
 
-
-    init_inst(0xC1, POP, 3, 0, exec_pop_from_stack16);
-    init_inst(0xC5, PUSH, 4, 0, exec_push_to_stack16);
+    for (uint8_t pair = 0; pair < 4; pair++) {
+        init_inst((uint8_t)(0xC5 | (pair << 4)), PUSH, 4, 0, exec_push_rr);
+        init_inst((uint8_t)(0xC1 | (pair << 4)), POP,  3, 0, exec_pop_rr);
+    }
 }
 
 void instruction_set_init(void) {
