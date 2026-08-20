b870680c78054bbfc0feb8d60bb88d1b57bf3a88
leukelele <70491331+leukelele@users.noreply.github.com>
2026-08-20 11:20:52 -0500

    thinking of adding 16 bit register get and set

--- include/cpu.h
+++ include/cpu.h
@@ -84,6 +84,9 @@ uint8_t cpu_step(struct cpu *cpu);
 uint8_t reg8_get(struct cpu *cpu, uint8_t index);
 void reg8_set(struct cpu *cpu, uint8_t index, uint8_t value);
 
+uint16_t reg16_get(struct cpu *cpu, uint8_t index);
+void reg16_set(struct cpu *cpu, uint8_t index, uint8_t value);
+
 void stack_push16(struct cpu *cpu, uint16_t value);
 uint16_t stack_pop16(struct cpu *cpu);
 
